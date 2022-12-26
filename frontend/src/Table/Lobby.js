09bbc50474a6bf2592bafcc73da30cb646fc048f
Duc Minh Nguyen <56620618+danielbaker06072001@users.noreply.github.com>
2022-12-26 00:04:31 -0500

    finish main menu

---
+++ frontend/src/Table/Lobby.js
@@ -0,0 +1,22 @@
+import '../style/lobby.css';
+
+const Lobby = () => { 
+
+    function createGame() {
+
+    }
+
+    return ( 
+        <div className = "menu"> 
+            <div>
+                <button>Create Game</button>
+                <button>Join Game</button>
+                <button>Match Making</button>
+            </div>
+            
+        </div>
+    );
+
+};
+
+export default Lobby;
---
+++ frontend/src/Table/Table.js
@@ -0,0 +1,19 @@
+import '../style/table.css'
+
+
+const Table = () => { 
+
+    function test() { 
+    }
+
+    return(
+        <div className = "tableGrid">
+            <div className = "playerOne">asddsaddadf</div>
+            <div className = "playerTwo"></div>
+            <div className = "playerThree">dsadadsdsadasdanh</div>
+            <div className = "playerFour">f</div>
+        </div>
+        )
+};
+
+export default Table;
